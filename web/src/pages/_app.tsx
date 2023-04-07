d5a5023766e1ec4f95516ee9e5b4e87c5679ce72
Ging123 <iceglauber@outlook.com>
2023-04-07 17:04:36 -0300

    :rocket: Feature - Add loading

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
 
 import "../styles/global.scss";
@@ -5,6 +6,7 @@ import "../styles/global.scss";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextNProgress color="#f1bc0c"/>
       <Component { ...pageProps } />;
     </>
   );
